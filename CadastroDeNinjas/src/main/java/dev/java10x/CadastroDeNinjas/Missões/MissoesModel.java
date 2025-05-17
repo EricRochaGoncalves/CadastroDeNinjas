b5d852f86af73853ebee2e1656ac2c638472c433
Eric Gonçalves <ericrochagoncalves59@gmail.com>
2025-05-17 05:46:42 -0300

    Modelagem do banco de dados ninja model e missõesModels concluidos

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missões/MissoesModel.java
@@ -0,0 +1,28 @@
+package dev.java10x.CadastroDeNinjas.Missões;
+
+import java.util.List;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "tb_missoes")
+public class MissoesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+
+    
+
+    
+}
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,9 +1,13 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.hibernate.annotations.ManyToAny;
+
+import dev.java10x.CadastroDeNinjas.Missões.MissoesModel;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 
 // @Entity ele transforma uma classe em uma entidade no Banco de Dados
@@ -13,10 +17,15 @@ import jakarta.persistence.Table;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    @ManyToAny
+    @JoinColumn(name = "missoes_id")
+
+    private MissoesModel missoes;
     
 
     public NinjaModel() {
