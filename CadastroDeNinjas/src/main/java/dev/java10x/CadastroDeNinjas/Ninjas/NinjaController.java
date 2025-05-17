30dd555729ac5f0e2ab4471279c3fa6d2763d8b5
Eric Gonçalves <ericrochagoncalves59@gmail.com>
2025-05-17 06:06:24 -0300

    Commit final

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,5 +1,25 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ninjas")
 public class NinjaController {
-    
+
+    @Autowired
+    private NinjaRepository ninjaRepository;
+
+    @GetMapping
+    public List<NinjaModel> listarTodos() {
+        return ninjaRepository.findAll();
+    }
+
+    @PostMapping
+    public NinjaModel cadastrar(@RequestBody NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
 }
+
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NoArsConstructor.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NoArsConstructor.java
@@ -1,5 +1,12 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-public @interface NoArsConstructor {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+// Isso é só um placeholder pra evitar erro no seu projeto, já que o tutorial tá usando
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface NoArsConstructor {
 }
