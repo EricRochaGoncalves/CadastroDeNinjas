272e6563b9c05167f4e5768891346af6b0d797e0
Eric Gonçalves <ericrochagoncalves59@gmail.com>
2025-05-17 05:56:27 -0300

    Criando o @NoArgsConstructor

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Table;
 // @Entity ele transforma uma classe em uma entidade no Banco de Dados
 @Entity  
 @Table (name = "tb_cadastro") 
+@NoArsConstructor
 
 public class NinjaModel {
     @Id
@@ -28,46 +29,5 @@ public class NinjaModel {
     private MissoesModel missoes;
     
 
-    public NinjaModel() {
-
-    }
-
     
-
-    public NinjaModel(String nome, String email, int idade) {
-       
-
-        
-
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-
-    }
-
-    public String getNome() {
-        return nome;
-
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
 }
